636b71d80d81c2f9e9ecea7c09bcd9ec19e2d2a2
Matt Snelson <matt_git@sent.com>
2022-02-18 12:00:17 +1100

    init commit

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' @title addition
+#' @description This adds things
+#' @param a first bumber
+#' @param b second number
+#'
+#' @importFrom dplyr select
+
+
+addition <- function(a,b) {
+  return(a + b)
+}
